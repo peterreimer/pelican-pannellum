3d7c8b8b0375c3c0a4de70edd5cadceb8429d461
Peter Reimer <peter@4pi.org>
2015-06-04 23:10:26 +0200

    using defaults for some properties, switch from pano_id to scene_id

--- pannellum.py
+++ pannellum.py
@@ -22,6 +22,9 @@ logger = logging.getLogger(__name__)
 
 CONTENT_FOLDER = 'content'
 
+# setting up some defaults
+TILE_FOLDER = 'tiles'
+SIZES_FOLDER = 'sizes'
 
 class PannellumGenerator(Generator):
     """Generate XMLs on the output dir, for articles containing pano_ids"""
@@ -29,8 +32,22 @@ class PannellumGenerator(Generator):
     def __init__(self, *args, **kwargs):
         """doc"""
         super(PannellumGenerator, self).__init__(*args, **kwargs)
-        self.tile_folder = self.settings['TILE_FOLDER']
+        if 'TILE_FOLDER' not in self.settings: 
+            self.tile_folder = TILE_FOLDER
+        else:
+            self.tile_folder = self.settings['TILE_FOLDER']
+        
+        if 'SIZES_FOLDER' not in self.settings: 
+            self.sizes_folder = SIZES_FOLDER
+        else:
+            self.sizes_folder = self.settings['SIZES_FOLDER']
+
         self.json_folder = self.settings['JSON_FOLDER']
+        self.sizes  = {
+            'banner' : (1024, 256),
+            'preview' : (600, 200),
+            'icon': (150, 50)
+            }
         self.fullsize_panoramas  = os.path.join(CONTENT_FOLDER, self.settings['FULLSIZE_PANORAMAS'])
         if not os.path.isdir(self.fullsize_panoramas):
             logger.warn("%s does not exist" % self.fullsize_panoramas)
@@ -38,25 +55,25 @@ class PannellumGenerator(Generator):
 
 
     def _create_json(self, obj, json_path, tile_path, base_path):
-        
-        if hasattr(obj,'pano_ids'):
-            output_json = os.path.join(json_path, obj.slug + ".js")
-            logger.info(output_json)            
-            pano_ids = [pano_id.strip() for pano_id in obj.pano_ids.split(',')]
+        if hasattr(obj,'scene_id'):
             panoramas = []
-            for pano_id in pano_ids:
-                panorama = os.path.join(self.fullsize_panoramas, pano_id + '.jpg')
-                if os.path.isfile(panorama):
-                    panoramas.append(panorama)
-                else:
-                    logger.error("%s does not exist" % panorama)
+            panorama = os.path.join(self.fullsize_panoramas, obj.scene_id + '.jpg')
+            if os.path.isfile(panorama):
+                panoramas.append(panorama)
+            else:
+                logger.error("%s does not exist" % panorama)
             
             if len(panoramas) > 0:
                 tour = Tour(author="Peter Reimer", debug=self.debug, tile_folder=tile_path, basePath=base_path, panoramas=panoramas)
                 for scene in tour.scenes:
                     scene.tile(force=False)
-
+                    sizes_path = os.path.join(CONTENT_FOLDER, self.sizes_folder, obj.scene_id)
+                    self._get_or_create_path(sizes_path)
+                    for name, size in self.sizes.iteritems():
+                        scene.sizes(name, size[0], size[1], sizes_folder=sizes_path)
                 # writing viewer configuration file
+                
+                output_json = os.path.join(tile_path, obj.scene_id, "tour.js")
                 f = open(output_json, 'w')
                 f.write(tour.get_json())
                 f.close
@@ -78,10 +95,9 @@ class PannellumGenerator(Generator):
         # since we write our own files
         json_path = os.path.join(self.output_path, self.json_folder)
         tile_path = os.path.join(CONTENT_FOLDER, self.tile_folder)
-        base_path = '../' + self.tile_folder
+        base_path = ''
         self._get_or_create_path(json_path)
         self._get_or_create_path(tile_path)
-        logger.warn(tile_path)
 
         for article in self.context['articles']:
             self._create_json(article, json_path, tile_path, base_path)
