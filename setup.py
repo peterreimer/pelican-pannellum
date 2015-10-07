452170c97de397d916da8487554c5bc2576e6bd9
Peter Reimer <peter@4pi.org>
2015-10-07 22:27:50 +0200

    fixed indentation

--- setup.py
+++ setup.py
@@ -6,26 +6,26 @@ def _read(*rnames):
 
 
 setup(name='pelican-pannellum',
-      version='0.1',
-      description='Pannellum Plugin for pelican',
-      long_description=_read('Readme.rst'),
-      py_modules=['pannellum'],
-      author='Peter Reimer',
-      author_email='peter@4pi.org',
-      url='https://github.com/peterreimer/pelican-pannellum',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'Programming Language :: Python :: 2.7'
-          ],
-      keywords=['pelican', 'pannellum', 'panorama'],
-      include_package_data = True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Pillow',
-          'fourpi.pannellum',
-          'pelican'
-          # -*- Extra requirements: -*-
-      ],
+    version='0.2dev',
+    description='Pannellum Plugin for pelican',
+    long_description=_read('Readme.rst'),
+    py_modules=['pannellum'],
+    author='Peter Reimer',
+    author_email='peter@4pi.org',
+    url='https://github.com/peterreimer/pelican-pannellum',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords=['pelican', 'pannellum', 'panorama'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Pillow',
+        'fourpi.pannellum',
+        'pelican'
+        # -*- Extra requirements: -*-
+    ],
 )
